24f8ec7d1c9724321aae94426f0bbe4a731782e1
AnaViolletta <127507726+AnaViolletta@users.noreply.github.com>
2024-06-27 10:52:02 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const botoes = document.querySelectorAll(".botao");
+
 const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo01 = new Date("2024-12-28T00:00:00");
